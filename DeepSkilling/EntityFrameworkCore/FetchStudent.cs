cf9f9e384d36f58333bcc8133f1229120e8573dd
Abhiram Yadav <154219077+abhiramyadav22@users.noreply.github.com>
2026-07-05 10:27:32 +0530

    Add files via upload

---
+++ DeepSkilling/EntityFrameworkCore/FetchStudent.cs
@@ -0,0 +1,10 @@
+using System;
+
+class FetchStudent
+{
+    static void Main()
+    {
+        Console.WriteLine(
+        "Student Retrieved");
+    }
+}
---
+++ DeepSkilling/EntityFrameworkCore/InsertStudent.cs
@@ -0,0 +1,10 @@
+using System;
+
+class InsertStudent
+{
+    static void Main()
+    {
+        Console.WriteLine(
+        "Student Added");
+    }
+}
