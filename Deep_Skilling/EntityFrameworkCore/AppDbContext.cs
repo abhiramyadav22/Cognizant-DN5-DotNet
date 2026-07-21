d731b6c9bf0cbd90efdf2e67feedfd4ddcfdc418
Abhiram Yadav <154219077+abhiramyadav22@users.noreply.github.com>
2026-07-21 14:28:31 +0530

    Delete Deep_Skilling/EntityFrameworkCore directory

--- Deep_Skilling/EntityFrameworkCore/AppDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-class AppDbContext : DbContext
-{
-    public DbSet<Student> Students
-    {
-        get; set;
-    }
-}
--- Deep_Skilling/EntityFrameworkCore/Student.cs
+++
@@ -1,8 +0,0 @@
-class Student
-{
-    public int Id { get; set; }
-
-    public string Name { get; set; }
-
-    public int Age { get; set; }
-}
